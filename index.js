7ffe94afa4e51ae565075f154f432b237023ebcc
RedLime <redlimerl@gmail.com>
2023-04-29 06:41:57 +0900

    check changed strings

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const path = require('path');
 
 async function run() {
-    let defaultData = {};
+    let defaultData = null;
+    let backupData = {};
     const languageData = {};
     const editableExist = {};
 
@@ -12,6 +13,7 @@ async function run() {
         const basePath = core.getInput('base-path');
         const fileSuffix = core.getInput('end-with');
         const editableSuffix = core.getInput('editable-suffix');
+        const backupSuffix = core.getInput('backup-suffix');
         const defaultName = core.getInput('default-language');
     
         core.info('loading base-path files...');
@@ -24,13 +26,14 @@ async function run() {
             let finalizeName = pathName.substring(0, pathName.length - fileSuffix.length);
             
             const isEditable = finalizeName.endsWith(editableSuffix);
-            if (isEditable) {
-                finalizeName = finalizeName.substring(0, finalizeName.length - editableSuffix.length);
-            }
+            if (isEditable) finalizeName = finalizeName.substring(0, finalizeName.length - editableSuffix.length);
+            
+            const isBackup = finalizeName.endsWith(backupSuffix);
+            if (isBackup) finalizeName = finalizeName.substring(0, finalizeName.length - backupSuffix.length);
     
-            const isDefault = finalizeName == defaultName;
+            const isDefault = !isBackup && finalizeName == defaultName;
     
-            core.info(`found '${pathName}'. (editable: ${isEditable}, default: ${isDefault})`);
+            core.info(`found '${pathName}'. (editable: ${isEditable}, default: ${isDefault}, backup: ${isBackup})`);
 
             core.info(`reading '${pathName}'...`);
             const langData = JSON.parse(fs.readFileSync(path.join(basePath, pathName), 'utf8'));
@@ -38,6 +41,8 @@ async function run() {
 
             if (isDefault) {
                 defaultData = langData;
+            } else if (isBackup) {
+                backupData = langData;
             } else {
                 if (editableExist[finalizeName] == undefined) editableExist[finalizeName] = false;
                 
@@ -67,8 +72,14 @@ async function run() {
             const resultData = {};
             for (const [key, value] of Object.entries(languageData[langName])) {
                 const defaultValue = defaultData[key];
+                const oldValue = backupData[key];
+
                 if (defaultValue) {
+                    // check same value
                     if (defaultValue == value) continue;
+
+                    // check outdated original value
+                    if (oldValue && defaultValue != oldValue) continue;
                 }
 
                 resultData[key] = value;
@@ -77,6 +88,11 @@ async function run() {
             fs.writeFileSync(path.join(basePath, langName + fileSuffix), JSON.stringify(resultData, null, 4), 'utf8');
             core.info(`done with update '${langName}'.`);
         }
+
+        core.info(`backup old default strings...`);
+        fs.writeFileSync(path.join(basePath, defaultData + backupSuffix + fileSuffix), JSON.stringify(defaultData, null, 4), 'utf8');
+        core.info(`done with backup default strings.`);
+
         core.info(`complete!`);
     } catch (error) {
         core.setFailed(error.message);
