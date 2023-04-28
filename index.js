23996beba3deb1c22595bfaa325d9d8cb2bfc7f3
RedLime <redlimerl@gmail.com>
2023-04-29 04:35:59 +0900

    fix octokit?

--- index.js
+++ index.js
@@ -6,30 +6,36 @@ const glob = require('@actions/glob');
 const fs = require('fs');
 const path = require('path');
 
-try {
-    const basePath = core.getInput('base-path');
-    const fileSuffix = core.getInput('end-with');
-    const editableSuffix = core.getInput('editable-suffix');
-    const defaultName = core.getInput('default-language');
-
-    core.debug('reading base-path files...');
-    const pathFiles = fs.readdirSync(path.join(basePath));
-    core.debug(`read ${pathFiles.length} base-path files.`);
-
-    for (const pathName of pathFiles) {
-        if (!pathName.endsWith(fileSuffix)) return;
-        
-        let finalizeName = pathName.substring(0, pathName.length - fileSuffix.length);
-        
-        const isEditable = finalizeName.endsWith(editableSuffix);
-        if (isEditable) {
-            finalizeName = finalizeName.substring(0, finalizeName.length - editableSuffix.length);
+async function run() {
+    try {
+        const basePath = getInput('base-path');
+        const fileSuffix = getInput('end-with');
+        const editableSuffix = getInput('editable-suffix');
+        const defaultName = getInput('default-language');
+    
+        core.debug('reading base-path files...');
+        const pathFiles = fs.readdirSync(path.join(basePath));
+        core.debug(`read ${pathFiles.length} base-path files.`);
+    
+        for (const pathName of pathFiles) {
+            if (!pathName.endsWith(fileSuffix)) return;
+            
+            let finalizeName = pathName.substring(0, pathName.length - fileSuffix.length);
+            
+            const isEditable = finalizeName.endsWith(editableSuffix);
+            if (isEditable) {
+                finalizeName = finalizeName.substring(0, finalizeName.length - editableSuffix.length);
+            }
+    
+            const isDefault = finalizeName == defaultName;
+    
+            core.debug(`found '${pathName}'. (editable: ${isEditable}, default: ${isDefault})`);
         }
-
-        const isDefault = finalizeName == defaultName;
-
-        core.debug(`found '${pathName}'. (editable: ${isEditable}, default: ${isDefault})`);
+    } catch (error) {
+        core.setFailed(error.message);
     }
-} catch (error) {
-    core.setFailed(error.message);
+
+
 }
+
+run();
